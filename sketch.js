ca89e50016874dc90ab2ccbffe8d00b9c9c6a2c4
Gabe Thin <gathin@packer.edu>
2021-05-12 14:18:09 -0400

    animation

--- sketch.js
+++ sketch.js
@@ -1,16 +1,11 @@
 function setup() {
   createCanvas(600, 600);
-  let size = 150
-  let cell = new Cell(200, 200, 100)
-  let chrom1a = new Chromosome(width/2-(0.15*size), width/2-(0.15*size), size/10, "blue", "A")
-  let chrom1b = new Chromosome(width/2-(0.15*size), width/2+(0.15*size), size/20, "blue", "B")
-  let chrom2a = new Chromosome(width/2+(0.15*size), width/2-(0.15*size), size/10, "red", "a")
-  let chrom2b = new Chromosome(width/2+(0.15*size), width/2+(0.15*size), size/20, "red", "b")
-  stroke("red");
-  text("Red = parent 1 chromosomes", 10, 15);
-  stroke("blue");
-  text("Blue = parent 2 chromosomes", 10, 30);
-  let stage = "late_phosphase";
+  size = 150;
+  cell = new Cell(width / 2, width / 2, size)
+  chrom1a = new Chromosome(width/2-(0.15*size), width/2-(0.15*size), size/10, "blue", "A")
+  chrom1b = new Chromosome(width/2-(0.15*size), width/2+(0.15*size), size/20, "blue", "B")
+  chrom2a = new Chromosome(width/2+(0.15*size), width/2-(0.15*size), size/10, "red", "a")
+  chrom2b = new Chromosome(width/2+(0.15*size), width/2+(0.15*size), size/20, "red", "b")
 }
 
 class Chromosome {
@@ -22,9 +17,12 @@ class Chromosome {
     this.allele = allele;
   }
   draw(){
-    stroke(this.parent)
-    line(this.x+this.size, this.y+this.size, this.x-this.size, this.y-this.size)
-    line(this.x-this.size, this.y+this.size, this.x+this.size, this.y-this.size)
+    stroke(this.parent);
+    line(this.x+this.size, this.y+this.size, this.x-this.size, this.y-this.size);
+    line(this.x-this.size, this.y+this.size, this.x+this.size, this.y-this.size);
+    stroke(0);
+    line(this.x-(this.size/1.5), this.y-(this.size/10), this.x-(this.size/10), this.y-(this.size/1.5));
+    text(this.allele, this.x-(this.size), this.y-(this.size));
   }
 
 }
@@ -38,41 +36,43 @@ class Cell {
   draw(){
     stroke("black")
     ellipse(this.x, this.y, this.size, this.size)
-
   }
-
 }
 
 
 function draw() {
   background("white");
-  strokeWeight(5)
+  strokeWeight(2)
   show()
+  stroke("red");
+  text("Red = parent 1 chromosomes", 10, 15);
+  stroke("blue");
+  text("Blue = parent 2 chromosomes", 10, 30);
+  stage = "prophase";
 
-//- -    Late_Phosphase_1
-  if (stage == "late_phosphase"){
-    if (stage == "late_phosphase" && chrom1b.x != 185){
-      chrom1b.x = chrom1b.x- 1
-      chrom1b.y = chrom1b.y+ 1
-      chrom2a.x =chrom2a.x+ 1
-      chrom2a.y =chrom2a.y- 1
+//- -    Prophase
+  if (stage == "prophase"){
+    if (chrom1b.x <= width / 2 - (1.15 * chrom1b.size)) {
+      chrom1b.x += 1;
+      chrom2b.x -= 1;
+      chrom1a.x += 1;
+      chrom2a.x -= 1;
+
     }
-    else if(chrom1b.x == 185) {
-      print (chrom1b.x)
+    else if(chrom1b.x > width / 2 - (1.15 * chrom1b.size)) {
       stage = "metaphase"
-    }}
-//-- Metaphase
-  if (stage == "metaphase"){
-    if (chrom2a.x != 205){
-      chrom1a =  chrom1a +1
-      chrom1b =  chrom1b+1
-      chrom2a = chrom2a-1
-      chrom2b = chrom2b-1
     }
-
   }
+// //-- Metaphase
+//   if (stage == "metaphase"){
+//     if (chrom2a.x != 205){
+//       chrom1a =  chrom1a +1
+//       chrom1b =  chrom1b+1
+//       chrom2a = chrom2a-1
+//       chrom2b = chrom2b-1
+//     }
 
-
+  // }
 
 }
 
