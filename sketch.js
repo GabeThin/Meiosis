89eb9c84ce03e348e39aa18512d48d6345c62bbe
Gabe Thin <gathin@packer.edu>
2021-05-12 14:01:12 -0400

    sketch

--- sketch.js
+++ sketch.js
@@ -1,26 +1,32 @@
 function setup() {
-  createCanvas(400, 400);
-  cell = new Cell(200, 200, 100)
-  chrom1a = new Chromosome(185, 185, 10, "blue")
-  chrom1b = new Chromosome(215, 185, 5, "blue")
-  chrom2a = new Chromosome(185, 215, 10, "red")
-  chrom2b = new Chromosome(215, 215, 5, "red")
-  var stage = "late_phosphase";
+  createCanvas(600, 600);
+  let size = 150
+  let cell = new Cell(200, 200, 100)
+  let chrom1a = new Chromosome(width/2-(0.15*size), width/2-(0.15*size), size/10, "blue", "A")
+  let chrom1b = new Chromosome(width/2-(0.15*size), width/2+(0.15*size), size/20, "blue", "B")
+  let chrom2a = new Chromosome(width/2+(0.15*size), width/2-(0.15*size), size/10, "red", "a")
+  let chrom2b = new Chromosome(width/2+(0.15*size), width/2+(0.15*size), size/20, "red", "b")
+  stroke("red");
+  text("Red = parent 1 chromosomes", 10, 15);
+  stroke("blue");
+  text("Blue = parent 2 chromosomes", 10, 30);
+  let stage = "late_phosphase";
 }
 
 class Chromosome {
-  constructor(x, y, size, parent) {
+  constructor(x, y, size, parent, allele) {
     this.x = x;
     this.y = y;
     this.size = size;
     this.parent = parent;
+    this.allele = allele;
   }
   draw(){
     stroke(this.parent)
     line(this.x+this.size, this.y+this.size, this.x-this.size, this.y-this.size)
     line(this.x-this.size, this.y+this.size, this.x+this.size, this.y-this.size)
   }
-    
+
 }
 
 class Cell {
@@ -32,9 +38,9 @@ class Cell {
   draw(){
     stroke("black")
     ellipse(this.x, this.y, this.size, this.size)
-    
+
   }
-  
+
 }
 
 
@@ -42,7 +48,7 @@ function draw() {
   background("white");
   strokeWeight(5)
   show()
-  
+
 //- -    Late_Phosphase_1
   if (stage == "late_phosphase"){
     if (stage == "late_phosphase" && chrom1b.x != 185){
@@ -63,14 +69,14 @@ function draw() {
       chrom2a = chrom2a-1
       chrom2b = chrom2b-1
     }
-    
+
   }
- 
-  
-  
+
+
+
 }
 
-  
+
 function show(){
   cell.draw()
   chrom1a.draw()
